b236acd6c1b6ed2ed4cc3b839e510dc351d4dfb0
Andrey Krivopalov <info@ilab.pro>
2015-07-16 15:07:06 +0600

    hd

---
+++ TENCAR/leftmenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  leftmenuTableViewController.h
+//  TENCAR
+//
+//  Created by ILAB PRO on 16.07.15.
+//  Copyright © 2015 ilab.pro LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface leftmenuTableViewController : UIViewController
+
+@end

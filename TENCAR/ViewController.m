8e5b5e549d2e03468462d27d2f9d86941378084e
ILAB PRO <info@ilab.pro>
2015-10-01 18:35:49 +0600

    hjjjdfdf

--- TENCAR/ViewController.m
+++ TENCAR/ViewController.m
@@ -9,11 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *test;
+@property (nonatomic, retain) IBOutlet UIImageView *test;
 
 @end
 
 @implementation ViewController
+
+@synthesize test;
+
+int n=0;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,6 +27,7 @@
     //imgView.contentMode = UIViewContentModeCenter;
     
     //[self.view addSubview:imgView];
+   /*
     NSMutableArray *images = [[NSMutableArray alloc] init];
     for (int i = 1; i <= 125; i++) {
         if(i>99)
@@ -40,23 +45,88 @@
         
     }
     
-    /*
-    self.test.image = [UIImage animatedImageWithImages:images duration:2.0f];
-   */
+    */
     
-    double delayInSeconds = 1.0;
+    
+    //self.images = [self creatAnimation:@"loader_anim"];
+    //test.image = [UIImage animatedImageWithImages:[self creatAnimation:@"loader_anim"] duration:2.0f];
+   
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:.04
+                                                      target:self
+                                                    selector:@selector(tiktak)
+                                                    userInfo:nil repeats:YES];
+    
+    
+    
+    
+    
+    /*
+    test.animationImages = [self creatAnimation:@"loader_anim"];
+    test.animationDuration = 2.0f;
+    test.animationRepeatCount = 0;
+    [test startAnimating];
+    */
+    
+    
+    double delayInSeconds = 5.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         [self performSegueWithIdentifier:@"goHome" sender:self];
+        
     });
+    
     
    
    
 }
+-(void)tiktak {
+    n++;
+    
+    if(n>9)
+    {
+        test.image = [UIImage imageNamed:
+                      [NSString stringWithFormat:@"TenCar_animation400%i.png", n]];
+    }
+    else
+    {
+        test.image = [UIImage imageNamed:
+                      [NSString stringWithFormat:@"TenCar_animation4000%i.png", n]];
+    }
+    if(n == 64) n = 0;
+}
 
+/*
+
+- (NSArray*)creatAnimation:(NSString*)fileName {
+  
+    UIImage *image = [UIImage imageNamed:fileName];
+    NSMutableArray *animationImages = [NSMutableArray array];
+    
+    int i_current = 0;
+    int i_have_frames = 125;
+    for (int i2 = 0; i2<11; i2++) {
+        
+        for (int i = 0; i<12; i++) {
+            i_current++;
+            CGImageRef imageRef = CGImageCreateWithImageInRect(image.CGImage,
+                                                               CGRectMake(i*300.0f, i2*300.0f, 300.0f, 300.0f));
+            UIImage *animationImage = [UIImage imageWithCGImage:imageRef];
+            [animationImages addObject:animationImage];
+            CGImageRelease(imageRef);
+            if(i_have_frames==i_current) break;
+        }
+        
+    }
+   
+    
+    return animationImages;
+
+}
+*/
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+   
 }
 
 @end
